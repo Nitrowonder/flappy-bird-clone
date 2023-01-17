9a94f79c6fb70925c43d861438599f19f564d0f8
Zach <zmjones14@gmail.com>
2023-01-17 14:13:57 -0600

    Added gravity and shiz

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
-
 import Phaser from "phaser";
 
 const config = {
+  // WEbGL, web graphics JS Api for rendering 2D and 3D grpahics
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
+    // Arcade physics plugin manages physics simulation
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: {
+        y: 200
+      }
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update,
   }
-};
+}
+
+// Loading assets, such as images, music, animation, etc
+function preload() {
+  this.load.image('sky-background', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+}
+
+let bird = null;
+let totalDelta = null;
+
+function create() {
+ this.add.image(0, 0, 'sky-background').setOrigin(0);
+ bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+}
+
+// 60fps
+function update(time, delta) {
+  totalDelta += delta;
+
+  if (totalDelta < 1000) {
+    return;
+  }
+  console.log(bird.body.velocity.y);
+  totalDelta = 0;
+
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
