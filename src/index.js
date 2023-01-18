9caa8f21fb27d2ab7b2d1d8f81d6378293f751b8
Zach <zmjones14@gmail.com>
2023-01-18 12:33:13 -0600

    Added moving pipes

--- src/index.js
+++ src/index.js
@@ -10,9 +10,6 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: {
-        y: 400
-      }
     }
   },
   scene: {
@@ -22,37 +19,73 @@ const config = {
   }
 }
 
-// Loading assets, such as images, music, animation, etc
-function preload() {
-  this.load.image('sky-background', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
 const velocity = 200;
+const pipesToRender = 4;
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
 
 let bird = null;
+let pipeHorizontalDistance = 0;
+
+const pipeVerticalDistanceRange = [150, 250];
+
+
+// Loading assets, such as images, music, animation, etc
+function preload() {
+  this.load.image('sky-background', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
+}
 
 function create() {
   this.add.image(0, 0, 'sky-background').setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
+
+  for (let i = 0; i < pipesToRender; i++) {
+    pipeHorizontalDistance += 400;
+
+    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
+  }
+
+
+  //bird.body.velocity.x = velocity;
+  bird.body.gravity.y = 400;
+
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
-  bird.body.velocity.x = velocity;
 }
 
 // 60fps
 function update(time, delta) {
-  if(bird.x >= config.width - bird.width) {
-    bird.body.velocity.x = -velocity;
-  }
-  if (bird.x <= 0) {
-    bird.body.velocity.x = velocity;
-  }
+  // if(bird.x >= config.width - bird.width) {
+  //   bird.body.velocity.x = -velocity;
+  // }
+  // if (bird.x <= 0) {
+  //   bird.body.velocity.x = velocity;
+  // }
 
   if (bird.y > config.height || bird.y < 0 - bird.height) {
     restartBirdPosition();
   }
+}
+
+function placePipe(uPipe, lPipe) {
+  pipeHorizontalDistance += 400;
+  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+
+  uPipe.body.velocity.x = -200;
+  lPipe.body.velocity.x = -200;
 }
 
 function restartBirdPosition() {
