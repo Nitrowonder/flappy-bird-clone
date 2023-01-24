b457d1292c2e26502ab97048d4f26e7ff8b1dede
Zach <zmjones14@gmail.com>
2023-01-24 12:39:51 -0600

    Added game difficulty scaling

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -13,17 +13,34 @@ class PlayScene extends BaseScene {
 
         this.pipeHorizontalDistance = 0;
 
-        this.pipeVerticalDistanceRange = [150, 250];
-        this.pipeHorizontalDistanceRange = [500, 550];
+        // this.pipeVerticalDistanceRange = [150, 250];
+        // this.pipeHorizontalDistanceRange = [500, 550];
 
         this.flapVelocity = 300;
 
         this.score = 0;
         this.scoreText = '';
 
+        this.currentDifficulty = 'easy';
+        this.difficulties = {
+            'easy': {
+                pipeHorizontalDistanceRange: [300, 350],
+                pipeVerticalDistanceRange: [150, 200]
+            },
+            'normal': {
+                pipeHorizontalDistanceRange: [280, 330],
+                pipeVerticalDistanceRange: [140, 190]
+            },
+            'hard': {
+                pipeHorizontalDistanceRange: [250, 310],
+                pipeVerticalDistanceRange: [120, 170]
+            }
+        }
+
     }
 
     create() {
+        this.currentDifficulty = 'easy';
         super.create();
         this.createBird();
         this.createPipes();
@@ -134,10 +151,11 @@ class PlayScene extends BaseScene {
     }
 
     placePipe(uPipe, lPipe) {
+        const difficulty = this.difficulties[this.currentDifficulty];
         const rightMostX = this.getRightMostPipe();
-        const pipeVerticalDistance = Phaser.Math.Between(...this.pipeVerticalDistanceRange);
+        const pipeVerticalDistance = Phaser.Math.Between(...difficulty.pipeVerticalDistanceRange);
         const pipeVerticalPosition = Phaser.Math.Between(0 + 20, this.config.height - 20 - pipeVerticalDistance);
-        const pipeHorizontalDistance = Phaser.Math.Between(...this.pipeHorizontalDistanceRange    );
+        const pipeHorizontalDistance = Phaser.Math.Between(...difficulty.pipeHorizontalDistanceRange    );
       
         uPipe.x = rightMostX + pipeHorizontalDistance;
         uPipe.y = pipeVerticalPosition;
@@ -156,9 +174,19 @@ class PlayScene extends BaseScene {
                 this.placePipe(...tempPipes);
                 this.increaseScore();
                 this.saveBestScore();
+                this.increaseDifficulty();
             }
             }
         }))
+    }
+
+    increaseDifficulty() {
+        if (this.score === 2) {
+            this.currentDifficulty = 'normal';
+        }
+        if (this.score === 4) {
+            this.currentDifficulty = 'hard';
+        }
     }
 
     getRightMostPipe() {
